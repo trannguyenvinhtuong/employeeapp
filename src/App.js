97fd9717c98c4f95cb5407a824d7be43cfc4a581
ShinspireMichaelTran <77633711+trannguyenvinhtuong@users.noreply.github.com>
2025-09-04 16:32:43 +0700

    change route

--- src/App.js
+++ src/App.js
@@ -1,34 +1,32 @@
-import {Component} from 'react';
-import Home from './components/home/Home';
-import Navcp from './components/home/Navcp';
-import './Style.css';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import routes from './routes';
+import { Component } from "react";
+import Home from "./components/home/Home";
+import Navcp from "./components/home/Navcp";
+import "./Style.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import routes from "./routes";
 
 class App extends Component {
-  showContent = (routes) =>{
+  showContent = (routes) => {
     var rs = null;
-    if(routes.length>0){
-      rs=routes.map((route)=>{
-        return(
+    if (routes.length > 0) {
+      rs = routes.map((route) => {
+        return (
           <Route
             key={route.id}
             path={route.path}
             exact={route.exact}
             component={route.main}
           />
-        )
-      })
+        );
+      });
     }
     return rs;
-}
+  };
   render() {
     return (
       <Router>
         <Navcp />
-        <Switch>
-          {this.showContent(routes)}
-        </Switch>
+        <Switch>{this.showContent(routes)}</Switch>
       </Router>
     );
   }
--- src/routes.js
+++ src/routes.js
@@ -1,26 +1,26 @@
-import Home from './components/home/Home';
-import Notfound from './components/Notfound';
-import Details from './components/detail/Details';
+import Home from "./components/home/Home";
+import Notfound from "./components/Notfound";
+import Details from "./components/detail/Details";
 
 const routes = [
-    {
-        id: 1,
-        path: '/',
-        exact: true,
-        main: () => <Home />
-    },
-    {
-        id: 2,
-        path: '/detail/:dataid',
-        exact: false,
-        main: (match) => <Details match = {match}/>
-    },
-    {
-        id: 3,
-        path: '',
-        exact: false,
-        main: () => <Notfound />
-    }
+  {
+    id: 1,
+    path: "/employeeapp",
+    exact: true,
+    main: () => <Home />,
+  },
+  {
+    id: 2,
+    path: "/detail/:dataid",
+    exact: false,
+    main: (match) => <Details match={match} />,
+  },
+  {
+    id: 3,
+    path: "",
+    exact: false,
+    main: () => <Notfound />,
+  },
 ];
 
 export default routes;
